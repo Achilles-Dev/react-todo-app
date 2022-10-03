e2eba66dca6494ab9ba34f61601eaecf373596a9
Achilles-Dev <solohag92@gmail.com>
2022-10-03 10:57:33 +0000

    Update SinglePage.js

--- src/functionBased/pages/SinglePage.js
+++ src/functionBased/pages/SinglePage.js
@@ -13,7 +13,7 @@ const SinglePage = () => {
       slug: 'about-author',
       title: 'About the Author',
       description:
-        'This app was developed by Ibas Majid, a self-taught web developer and a technical writer. He is opened to freelance Gig. So go ahead and connect with ibas on Twitter @ibaslogic.',
+        'This app was developed by Solomon Hagan, a self-taught web developer. He is open to freelance gigs. So go ahead and connect with solomon on Twitter @SoloAchilles.',
     },
   ];
   const { slug } = useParams();
